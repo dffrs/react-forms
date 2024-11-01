ee3841492ac7a87f0a24f21b723cd134b8ff7ee3
dffrs <dffrs@iscte-iul.pt>
2024-11-01 11:52:01 +0000

    create a new test case for useForm
    verify that form has a stable reference (Parent re-render test case)

--- src/test/useForm.test.tsx
+++ src/test/useForm.test.tsx
@@ -3,7 +3,7 @@ import { useForm } from "../form";
 import { render } from "@testing-library/react";
 
 describe("Form Tests: useForm", () => {
-  it("should always have the same reference", () => {
+  it("should always have the same reference (internal state)", () => {
     const DummyComp = () => {
       const [_, setS] = useState(false);
 
@@ -19,6 +19,30 @@ describe("Form Tests: useForm", () => {
       }, [form]);
 
       return null;
+    };
+
+    render(<DummyComp />);
+  });
+
+  it("should always have the same reference (Parent re-renders)", () => {
+    const ChildComp = ({ form }: { form: any }) => {
+      const internalRef = useRef(form);
+
+      expect(form).toBe(internalRef.current);
+      return null;
+    };
+
+    const DummyComp = () => {
+      const [_, setS] = useState(false);
+
+      const form = useForm("dummy-form");
+
+      useEffect(() => {
+        // Re-render by toggling the state
+        setS((prev) => !prev);
+      }, []);
+
+      return <ChildComp form={form}></ChildComp>;
     };
 
     render(<DummyComp />);
