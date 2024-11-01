23e4a4c8b3d1788f3517f4d29b5e039a5eace625
dffrs <dffrs@iscte-iul.pt>
2024-11-01 10:30:19 +0000

    created tests for all inputs
    Verify how form behaves when registers an input, without any
    value/default value injected

--- src/test/form.test.tsx
+++ src/test/form.test.tsx
@@ -1,18 +1,136 @@
 import { Form } from "../form";
 import { render } from "@testing-library/react";
 
-describe("Form tests", () => {
-  it("registers field input type text", () => {
-    const form = new Form("test");
+type InputType = React.HTMLInputTypeAttribute;
+type Temp = { descr: string; type: InputType; expected: unknown };
 
-    const InputComp = () => {
-      const ref = form.register("dummy-input");
-      return <input type="text" ref={ref} />;
-    };
+describe("Form tests: Values", () => {
+  (
+    [
+      {
+        descr: "type text",
+        type: "text",
+        expected: "",
+      },
+      {
+        descr: "type url",
+        type: "url",
+        expected: "",
+      },
+      {
+        descr: "type date",
+        type: "date",
+        expected: "",
+      },
+      {
+        descr: "type file",
+        type: "file",
+        expected: "",
+      },
+      {
+        descr: "type time",
+        type: "time",
+        expected: "",
+      },
+      {
+        descr: "type week",
+        type: "week",
+        expected: "",
+      },
+      {
+        descr: "type color",
+        type: "color",
+        expected: "#000000",
+      },
+      {
+        descr: "type email",
+        type: "email",
+        expected: "",
+      },
+      {
+        descr: "type image",
+        type: "image",
+        expected: "",
+      },
+      {
+        descr: "type month",
+        type: "month",
+        expected: "",
+      },
+      {
+        descr: "type radio",
+        type: "radio",
+        expected: "on",
+      },
+      {
+        descr: "type range",
+        type: "range",
+        expected: "50",
+      },
+      {
+        descr: "type reset",
+        type: "reset",
+        expected: "",
+      },
+      {
+        descr: "type number",
+        type: "number",
+        expected: "",
+      },
+      {
+        descr: "type button",
+        type: "button",
+        expected: "",
+      },
+      {
+        descr: "type hidden",
+        type: "hidden",
+        expected: "",
+      },
+      {
+        descr: "type search",
+        type: "search",
+        expected: "",
+      },
+      {
+        descr: "type hidden",
+        type: "hidden",
+        expected: "",
+      },
+      {
+        descr: "type submit",
+        type: "submit",
+        expected: "",
+      },
+      {
+        descr: "type checkbox",
+        type: "checkbox",
+        expected: "on",
+      },
+      {
+        descr: "type password",
+        type: "password",
+        expected: "",
+      },
+      {
+        descr: "type datetime-local",
+        type: "datetime-local",
+        expected: "",
+      },
+    ] as Array<Temp>
+  ).forEach(({ descr, type, expected }) => {
+    it("registers field input " + descr, () => {
+      const form = new Form("test");
 
-    render(<InputComp />);
+      const InputComp = () => {
+        const ref = form.register("dummy-input");
+        return <input type={type} ref={ref} />;
+      };
 
-    expect(form.getValues()).toEqual({ ["dummy-input"]: "" });
+      render(<InputComp />);
+
+      expect(form.getValues()).toEqual({ ["dummy-input"]: expected });
+    });
   });
 
   it("respects inptus value", () => {
