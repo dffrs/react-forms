befa6c732afd886272402f9885395e705eca5bca
dffrs <dffrs@iscte-iul.pt>
2024-11-02 18:29:45 +0000

    deleted boilerplate files and display a few inputs (for testing purposes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from "./form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const form = useForm("inputs", {
+    defaultValues: { "i-checkbox": true, "i-text": "this is a text input" },
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Inputs</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          flexDirection: "column",
+          gap: "10px",
+          width: "100vw",
+        }}
+      >
+        <div>
+          <h3>Text</h3>
+          <input type="text" id="textId" {...form.register("i-text")} />
+        </div>
+        <div>
+          <h3>Checkbox</h3>
+          <input
+            type="checkbox"
+            id="checkboxId"
+            {...form.register("i-checkbox")}
+          />
+          <label htmlFor="checkboxId">Test</label>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
