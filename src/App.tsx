0bb2df1a9ad794db8e628c744269aa6dd46da777
dffrs <dffrs@iscte-iul.pt>
2024-11-05 21:21:20 +0000

    created a open/close drawer for manual tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm, useWatchValue } from "./form";
 
 function App() {
+  const [open, setOpen] = useState(() => true);
+
   const form = useForm("inputs", {
     defaultValues: {
       "i-checkbox": true,
@@ -38,58 +40,63 @@ function App() {
       >
         change checkbox
       </button>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "start",
-          flexDirection: "column",
-          gap: "10px",
-          width: "100vw",
-        }}
-      >
-        <div>
-          <h3>Text</h3>
-          <input type="text" id="textId" {...form.register("i-text")} />
+      <button type="button" onClick={() => setOpen((e) => !e)}>
+        open/close
+      </button>
+      {open && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            flexDirection: "column",
+            gap: "10px",
+            width: "100vw",
+          }}
+        >
+          <div>
+            <h3>Text</h3>
+            <input type="text" id="textId" {...form.register("i-text")} />
+          </div>
+          <div>
+            <h3>Checkbox</h3>
+            <input
+              type="checkbox"
+              id="checkboxId"
+              {...form.register("i-checkbox")}
+            />
+            <label htmlFor="checkboxId">Test</label>
+          </div>
+          <div>
+            <h3>Radio</h3>
+            <input
+              type="radio"
+              id="contactChoice1"
+              value="email"
+              {...form.register({ groupName: "i-radio", element: "email" })}
+            />
+            <label htmlFor="contactChoice1">Email</label>
+            <input
+              type="radio"
+              id="contactChoice2"
+              value="phone"
+              {...form.register({ groupName: "i-radio", element: "phone" })}
+            />
+            <label htmlFor="contactChoice2">Phone</label>
+            <input
+              type="radio"
+              id="contactChoice3"
+              value="mail"
+              {...form.register({ groupName: "i-radio", element: "mail" })}
+            />
+            <label htmlFor="contactChoice3">Mail</label>
+          </div>
+          <div>
+            <h3>File</h3>
+            <input type="file" id="fileId" {...form.register("i-file")} />
+            <label htmlFor="fileId">Test</label>
+          </div>
         </div>
-        <div>
-          <h3>Checkbox</h3>
-          <input
-            type="checkbox"
-            id="checkboxId"
-            {...form.register("i-checkbox")}
-          />
-          <label htmlFor="checkboxId">Test</label>
-        </div>
-        <div>
-          <h3>Radio</h3>
-          <input
-            type="radio"
-            id="contactChoice1"
-            value="email"
-            {...form.register({ groupName: "i-radio", element: "email" })}
-          />
-          <label htmlFor="contactChoice1">Email</label>
-          <input
-            type="radio"
-            id="contactChoice2"
-            value="phone"
-            {...form.register({ groupName: "i-radio", element: "phone" })}
-          />
-          <label htmlFor="contactChoice2">Phone</label>
-          <input
-            type="radio"
-            id="contactChoice3"
-            value="mail"
-            {...form.register({ groupName: "i-radio", element: "mail" })}
-          />
-          <label htmlFor="contactChoice3">Mail</label>
-        </div>
-        <div>
-          <h3>File</h3>
-          <input type="file" id="fileId" {...form.register("i-file")} />
-          <label htmlFor="fileId">Test</label>
-        </div>
-      </div>
+      )}
     </>
   );
 }
