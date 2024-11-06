b63ac4b6a3d84c6e78af747e872afe567691a23d
dffrs <dffrs@iscte-iul.pt>
2024-11-06 20:16:24 +0000

    [wip] test for useWatchValue

--- src/test/useWatchValue.test.tsx
+++ src/test/useWatchValue.test.tsx
@@ -1,27 +1,58 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useEffect } from "react";
+import { useWatchValue } from "../form";
 import { useForm } from "../form/hooks/useForm";
-import { Form, useWatchValue } from "../form";
 
 describe("Form Test: useWatchValue", () => {
   it("listens to value change", () => {
-    const ChildComp = ({ form }: { form: Form }) => {
+    const InputComp = () => {
+      const form = useForm("test-form", {
+        defaultValues: {
+          "text-input": "value from defaultValue",
+        },
+      });
+
       const value = useWatchValue("text-input", { form });
 
-      // @ts-ignore
-      return <span>{value}</span>;
-    };
+      useEffect(() => {
+        console.log("values", form.getValueFor("text-input"));
+      }, [form]);
 
-    const InputComp = () => {
-      const form = useForm("test-form");
+      useEffect(() => {
+        console.log("here", value);
+      }, [value]);
+
+      if (value instanceof FileList) throw Error();
 
       return (
         <>
-          <input type="text" {...form.register("text-input")} />
-          <ChildComp form={form} />
+          <input
+            data-testid="input"
+            type="text"
+            {...form.register("text-input")}
+          />
+          {/* @ts-expect-error */}
+          <span data-testid="span">{value}</span>
+          <button
+            data-testid="button"
+            onClick={() =>
+              form.setValueFor("text-input", "change with set value")
+            }
+          >
+            click
+          </button>
         </>
       );
     };
 
     render(<InputComp />);
+    const button = screen.getByTestId("button");
+    const span = screen.getByTestId("span");
+
+    expect(span).toHaveTextContent("value from defaultValue");
+
+    fireEvent.click(button);
+
+    expect(span).toHaveTextContent("change with set value");
   });
 });
