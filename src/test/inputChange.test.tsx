a0ff9827b5732eff6290ebfe18b9d1ba441204b9
dffrs <dffrs@icloud.com>
2024-11-17 17:00:34 +0000

    implemented the same test for checkbox

--- src/test/inputChange.test.tsx
+++ src/test/inputChange.test.tsx
@@ -2,7 +2,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import { useForm } from "../form";
 
 describe("On input change", () => {
-  it("input's input event triggers form to update it's values", () => {
+  it("input's input event triggers form to update it's values (input text field)", () => {
     const InputComp = () => {
       const form = useForm("test-form", {
         defaultValues: {
@@ -40,6 +40,46 @@ describe("On input change", () => {
     fireEvent.input(input, { target: { value: "this is the new text" } });
 
     expect(input).toHaveValue("this is the new text");
+
+    fireEvent.click(button);
+  });
+
+  it("input's input event triggers form to update it's values (input checkbox field)", () => {
+    const InputComp = () => {
+      const form = useForm("test-form", {
+        defaultValues: {
+          "checkbox-input": true,
+        },
+      });
+
+      return (
+        <>
+          <input
+            data-testid="input"
+            type="checkbox"
+            {...form.register("checkbox-input")}
+          />
+          <button
+            data-testid="button"
+            onClick={() =>
+              expect(form.getValueFor("checkbox-input")).toEqual(false)
+            }
+          >
+            click
+          </button>
+        </>
+      );
+    };
+
+    render(<InputComp />);
+    const input = screen.getByTestId("input");
+    const button = screen.getByTestId("button");
+
+    expect(input).toBeChecked();
+
+    fireEvent.click(input);
+
+    expect(input).not.toBeChecked();
 
     fireEvent.click(button);
   });
