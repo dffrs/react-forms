57f3b357e3b42022edeb22910d9dd873c330b295
dffrs <dffrs@icloud.com>
2025-05-23 23:34:22 +0100

    added test case for auto-injecting values (register function)

--- src/test/register.test.tsx
+++ src/test/register.test.tsx
@@ -1,6 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "../form";
-import { render } from "@testing-library/react";
+import {
+  fireEvent,
+  getByTestId,
+  queryByTestId,
+  render,
+} from "@testing-library/react";
 
 type InputType = React.HTMLInputTypeAttribute;
 type Temp = { descr: string; type: InputType; expected: unknown };
@@ -144,5 +149,62 @@ describe("Form tests: Values", () => {
     };
 
     render(<InputComp />);
+  });
+
+  it("auto-injecting values after calling register for the second time", async () => {
+    const InputComp = () => {
+      const [visible, setVisible] = useState(() => true);
+      const form = useForm("test", {
+        defaultValues: { "dummy-input": "test" },
+      });
+
+      useEffect(() => {
+        expect(form.getValues()).toEqual({ ["dummy-input"]: "test" });
+      }, [form]);
+
+      return (
+        <>
+          <button data-testid="button" onClick={() => setVisible((v) => !v)}>
+            click me
+          </button>
+          {visible && (
+            <input
+              data-testid="input"
+              type="text"
+              {...form.register("dummy-input")}
+            />
+          )}
+        </>
+      );
+    };
+
+    const { container } = render(<InputComp />);
+
+    const input = getByTestId(container, "input");
+    if (!input) throw Error("input not found");
+
+    const button = getByTestId(container, "button");
+    if (!button) throw Error("button not found");
+
+    // default value should be present on input
+    expect(input).toHaveValue("test");
+
+    // click on button to remove input from DOM
+    fireEvent.click(button);
+    expect(queryByTestId(container, "input")).toBeNull();
+
+    // click again and validate that input gets rendered with default value
+    fireEvent.click(button);
+    expect(input).toHaveValue("test");
+
+    // now lets do it again but changing the value to something else first
+    fireEvent.change(input, { target: { value: "new value" } });
+
+    expect(input).toHaveValue("new value");
+
+    fireEvent.click(button);
+    expect(queryByTestId(container, "input")).toBeNull();
+
+    expect(input).toHaveValue("new value");
   });
 });
