cb2d33da197bd38f28ba1cb1abd02eb53ed2bfb7
dffrs <dffrs@icloud.com>
2025-05-25 17:28:23 +0100

    added 1 more test case

--- src/test/resetDefaultValues.test.tsx
+++ src/test/resetDefaultValues.test.tsx
@@ -119,4 +119,51 @@ describe("Reset to Default Values tests", () => {
     expect(input).toHaveValue("test");
     expect(checkbox).toBeChecked();
   });
+
+  it("resetToDefaultValues does nothing if input didn't have any default value specified", async () => {
+    const InputComp = () => {
+      const form = useForm("test");
+
+      useEffect(() => {
+        expect(form.getValues()).toEqual({ "dummy-input": "" }); // no value
+      }, [form]);
+
+      return (
+        <>
+          <button
+            data-testid="button"
+            onClick={() => form.resetToDefaultValues("dummy-input")}
+          >
+            click me
+          </button>
+          <input
+            data-testid="input"
+            type="text"
+            {...form.register("dummy-input")}
+          />
+        </>
+      );
+    };
+
+    const { container } = render(<InputComp />);
+
+    const input = getByTestId(container, "input");
+    if (!input) throw Error("input not found");
+
+    const button = getByTestId(container, "button");
+    if (!button) throw Error("button not found");
+
+    // default value should be present on input
+    expect(input).toHaveValue("");
+
+    // input random text to input
+    fireEvent.change(input, { target: { value: "new value" } });
+    expect(input).toHaveValue("new value");
+
+    // click on button to reset values
+    fireEvent.click(button);
+
+    // validate that input still has the same value as before
+    expect(input).toHaveValue("new value");
+  });
 });
