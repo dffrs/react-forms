5c5bfbfc20b95ca1959131c103de5a2c6648cd21
dffrs <dffrs@icloud.com>
2025-05-25 17:25:07 +0100

    added new test case

--- src/test/resetDefaultValues.test.tsx
+++ src/test/resetDefaultValues.test.tsx
@@ -51,4 +51,72 @@ describe("Reset to Default Values tests", () => {
     // validate that input has default value again
     expect(input).toHaveValue("test");
   });
+
+  it("resetToDefaultValues (without fieldName) resets ALL values to default value", async () => {
+    const InputComp = () => {
+      const form = useForm("test", {
+        defaultValues: {
+          "dummy-input": "test",
+          "dummy-checkbox": true,
+        },
+      });
+
+      useEffect(() => {
+        expect(form.getValues()).toEqual({
+          ["dummy-input"]: "test",
+          ["dummy-checkbox"]: true,
+        });
+      }, [form]);
+
+      return (
+        <>
+          <button
+            data-testid="button"
+            onClick={() => form.resetToDefaultValues()}
+          >
+            click me
+          </button>
+          <input
+            data-testid="input"
+            type="text"
+            {...form.register("dummy-input")}
+          />
+          <input
+            data-testid="checkbox"
+            type="checkbox"
+            {...form.register("dummy-checkbox")}
+          />
+        </>
+      );
+    };
+
+    const { container } = render(<InputComp />);
+
+    const input = getByTestId(container, "input");
+    if (!input) throw Error("input not found");
+
+    const checkbox = getByTestId(container, "checkbox") as HTMLInputElement;
+    if (!checkbox) throw Error("checkbox not found");
+
+    const button = getByTestId(container, "button");
+    if (!button) throw Error("button not found");
+
+    // default value should be present on input & checkbox
+    expect(input).toHaveValue("test");
+    expect(checkbox).toBeChecked();
+
+    // input random text to input and uncheck checkbox
+    fireEvent.change(input, { target: { value: "new value" } });
+    fireEvent.click(checkbox);
+
+    expect(input).toHaveValue("new value");
+    expect(checkbox).not.toBeChecked();
+
+    // click on button to reset values
+    fireEvent.click(button);
+
+    // validate that input has default value again
+    expect(input).toHaveValue("test");
+    expect(checkbox).toBeChecked();
+  });
 });
