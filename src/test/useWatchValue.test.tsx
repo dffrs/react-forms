921942e04c7ce088dea02bdb4704ca2b2f452463
dffrs <dffrs@iscte-iul.pt>
2026-01-24 17:58:26 +0000

    added test case for useWatchValue for multi-select

--- src/test/useWatchValue.test.tsx
+++ src/test/useWatchValue.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { FormProvider, useWatchValue } from "../form";
+import { deepEqual, FormProvider, useWatchValue } from "../form";
 import { useForm } from "../form/hooks/useForm";
 
 describe("Form Test: useWatchValue", () => {
@@ -9,13 +9,16 @@ describe("Form Test: useWatchValue", () => {
         defaultValues: {
           "text-input": "value from defaultValue",
           "select-input": "option2",
+          "multi-select-input": ["option1", "option3"],
         },
       });
 
       const inputValue = useWatchValue("text-input", { form });
       const selectValue = useWatchValue("select-input", { form });
-
-      if (inputValue instanceof FileList) throw Error();
+      const multiSelectValue = useWatchValue("multi-select-input", {
+        form,
+        compareFn: deepEqual,
+      });
 
       return (
         <>
@@ -29,16 +32,30 @@ describe("Form Test: useWatchValue", () => {
             <option data-testid="option2">option2</option>
             <option data-testid="option3">option3</option>
           </select>
+          <select
+            data-testid="multi-select"
+            multiple
+            {...form.register("multi-select-input")}
+          >
+            <option data-testid="multi-option1">option1</option>
+            <option data-testid="multi-option2">option2</option>
+            <option data-testid="multi-option3">option3</option>
+          </select>
           {/* @ts-expect-error  value is unknow (for now, might improve in the future)*/}
           <span data-testid="span">{inputValue}</span>
 
           {/* @ts-expect-error  value is unknow (for now, might improve in the future)*/}
           <span data-testid="select-span">{selectValue}</span>
+
+          <span data-testid="multi-select-span">
+            {String(multiSelectValue)}
+          </span>
           <button
             data-testid="button"
             onClick={() => {
               form.setValueFor("text-input", "change with set value");
               form.setValueFor("select-input", "option3");
+              form.setValueFor("multi-select-input", ["option2"]);
             }}
           >
             click
@@ -51,14 +68,17 @@ describe("Form Test: useWatchValue", () => {
     const button = screen.getByTestId("button");
     const span = screen.getByTestId("span");
     const selectSpan = screen.getByTestId("select-span");
+    const multiSelectSpan = screen.getByTestId("multi-select-span");
 
     expect(span).toHaveTextContent("value from defaultValue");
     expect(selectSpan).toHaveTextContent("option2");
+    expect(multiSelectSpan).toHaveTextContent("option1,option3");
 
     fireEvent.click(button);
 
     expect(span).toHaveTextContent("change with set value");
     expect(selectSpan).toHaveTextContent("option3");
+    expect(multiSelectSpan).toHaveTextContent("option2");
   });
 
   it("listens to value change (no default value)", () => {
